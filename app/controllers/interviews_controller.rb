83b9bdc5ce5e70990d494968843edba0b5e586cd
Omansh Mathur <omansh@Omanshs-MacBook-Air.local>
2020-11-11 17:24:38 +0530

    CRUD added

--- app/controllers/interviews_controller.rb
+++ app/controllers/interviews_controller.rb
@@ -1,6 +1,35 @@
 class InterviewsController < ApplicationController
     def index
-    
+        @interviews = Interview.all
     end
-    
+    def new
+        @interview = Interview.new
+    end
+    def create
+        @interview = Interview.create(interview_params)
+        redirect_to interviews_path
+      end
+    def show
+        @interview = Interview.find(params[:id])
+      end
+    def edit
+        @interview = Interview.find(params[:id])
+      end
+    def update
+        @interview = Interview.find(params[:id])
+        @interview.update(interview_params)
+        redirect_to(interview_path(@interview))
+      end
+    def destroy
+        @interview = Interview.find(params[:id])
+        @interview.destroy
+        redirect_to interviews_path
+      end
+      private
+
+      def interview_params
+        params.require(:interview).permit(:interviewer_name, 
+        :interviewee_name, :interviewer_email, :interviewee_email, 
+        :interview_date, :interview_starttime, :interview_endtime)
+      end
 end
